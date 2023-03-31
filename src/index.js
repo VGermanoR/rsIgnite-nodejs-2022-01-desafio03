dcd43397a9a9804530275dbb014c2d7f0876df3d
Vitor Germano <vitorgermanoramos@gmail.com>
2023-03-31 15:23:11 -0300

    fix: delete repository

--- src/index.js
+++ src/index.js
@@ -48,15 +48,13 @@ app.put("/repositories/:id", (request, response) => {
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(
-    (repository) => repository.id === id
-  );
+  const index = repositories.findIndex((repository) => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (index === -1) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  repositories.splice(repositoryIndex, 1);
+  repositories.splice(index, 1);
 
   return response.status(204).send();
 });
